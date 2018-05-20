459d2872b210ad1b704d0f8be2c0d0c5c7e5d94c
jkramr <yuliya.kramarenko@gmail.com>
2018-05-20 17:48:24 +0300

    Initial commit

---
+++ src/main/java/com/jkramr/scbuildit/scbtools/ScbtoolsApplication.java
@@ -0,0 +1,12 @@
+package com.jkramr.scbuildit.scbtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScbtoolsApplication.class, args);
+	}
+}
